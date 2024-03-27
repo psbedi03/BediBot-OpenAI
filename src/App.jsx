080a0d1044ac77c80455420140e558c0055f4da9
psbedi03 <43196173+psbedi03@users.noreply.github.com>
2024-03-26 22:02:52 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import ChatBot from './Chat/ChatBot';
+
+function App() {
+
+
+
+
+  return (
+    <>
+      <ChatBot/>
+    </>
+  )
+}
+
+export default App
